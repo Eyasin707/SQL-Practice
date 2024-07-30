ab86104e1a3c4470df44b89f8c2017b6cec777c5
Eyasin Arafath <175514956+Eyasin707@users.noreply.github.com>
2024-07-31 00:38:05 +0600

    ES_01.sql

---
+++ ES_01.sql
@@ -0,0 +1,56 @@
+'''
+Write a solution to find all customers who never order anything.
+Return the result table in any order.
+The result format is in the following example.
+
+Example 1:
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+'''
+SELECT 
+c.name as customers
+FROM
+Customers as c
+LEFT JOIN
+Orders as o
+ON 
+c.id = o.customerId
+WHERE 
+o.customerId is null;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
